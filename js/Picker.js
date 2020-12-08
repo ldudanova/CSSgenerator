1848da6a0932257c9b8f587ad2863dc35c6b6835
ldudanova <l.dudanova92@gmail.com>
2020-12-08 18:49:12 +0300

    Add canvas Color picker

---
+++ js/Picker.js
@@ -0,0 +1,126 @@
+class Picker {
+    constructor(target, width, height) {
+        this.target = target;
+        this.width = width;
+        this.height = height;
+        this.target.width = width;
+        this.target.height = height;
+    //    Get context
+        this.context = this.target.getContext("2d");
+    //    Circle picker
+        this.pickerCircle = {x:10, y:10, width: 7, height:7};
+
+   this.eventListeners();
+
+    }
+
+    draw(){
+        this.build();
+    }
+
+    build(){
+        let gradient = this.context.createLinearGradient(0, 0, this.width, 0)
+
+        //    Color Stops
+        gradient.addColorStop(0, "rgb(255,0,0)");
+        gradient.addColorStop(0.15, "rgb(255,0,255)");
+        gradient.addColorStop(0.33, "rgb(0,255,255)");
+        gradient.addColorStop(0.49, "rgb(0,0,255)");
+        gradient.addColorStop(0.67, "rgb(0,255,0)");
+        gradient.addColorStop(0.84, "rgb(255,255,0)");
+        gradient.addColorStop(1, "rgb(255,0,0)");
+    //    Filling
+        this.context.fillStyle = gradient;
+        this.context.fillRect(0,0, this.width, this.height);
+
+    //    Apply black and whit
+        gradient = this.context.createLinearGradient(0,0,0, this.height);
+        gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+        gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
+        gradient.addColorStop(0.5, "rgba(0, 0, 0, 0)");
+        gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
+        this.context.fillStyle = gradient;
+        this.context.fillRect(0, 0, this.width, this.height);
+
+    //    Circle picker
+        this.context.beginPath();
+        this.context.arc(this.pickerCircle.x, this.pickerCircle.y, this.pickerCircle.width, 0, Math.PI * 2);
+        this.context.strokeStyle = "black";
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+        eventListeners() {
+            let isMouseDown = false;
+
+            const onMouseDown = (e) => {
+                let currentX = e.clientX - this.target.offsetLeft;
+                let currentY = e.clientY - this.target.offsetTop - 60;
+
+                if(currentY > this.pickerCircle.y
+                    && currentY < this.pickerCircle.y + this.pickerCircle.width
+                    && currentX > this.pickerCircle.x
+                    && currentX < this.pickerCircle.x + this.pickerCircle.width)
+                {
+                    isMouseDown = true;
+
+                } else {
+                    this.pickerCircle.x = currentX;
+                    this.pickerCircle.y = currentY;
+                    console.log("this.target.offsetLeft: "+this.target.offsetLeft);
+                    console.log("this.target.offsetTop: "+this.target.offsetTop);
+                    console.log("e.clientX: "+e.clientX);
+                    console.log("e.clientY: "+e.clientY);
+                    console.log("onMouseDown elseX: "+currentX);
+                    console.log("onMouseDown elseY: "+currentY);
+                    console.log("this.pickerCircle.x: "+this.pickerCircle.x);
+                    console.log("this.pickerCircle.y: "+this.pickerCircle.y);
+                }
+            }
+
+            const onMouseMove = (e) => {
+                    if(isMouseDown){
+                        let currentX = e.clientX - this.target.offsetLeft;
+                        let currentY = e.clientY - this.target.offsetTop;
+                        this.pickerCircle.x = currentX;
+                        this.pickerCircle.y = currentY;
+
+                    }
+            }
+
+            const onMouseUp = () => {
+                isMouseDown = false
+            }
+
+        //    Register actual mouse
+            this.target.addEventListener("mousedown", onMouseDown);
+            this.target.addEventListener("mousemove", onMouseMove);
+            this.target.addEventListener("mousemove", () => this.onChangeCallback(this.getPickedColor()));
+
+            document.addEventListener("mouseup", onMouseUp);
+        }
+
+        getPickedColor() {
+            let imageData = this.context.getImageData(this.pickerCircle.x, this.pickerCircle.y, 1,1);
+            return {
+                r: imageData.data[0],
+                g: imageData.data[1],
+                b:imageData.data[2]
+            }
+    }
+
+        onChange(callback) {
+            this.onChangeCallback = callback;
+        }
+}
+
+let picker = new Picker(document.getElementById("color-picker"), 250, 220);
+
+//Draw
+
+setInterval(() => picker.draw(), 0.1); //to loop dragging
+
+picker.onChange((color) => {
+    let selected = document.getElementsByClassName('js-selected-color')[0];
+    selected.style.background = `rgba(${color.r}, ${color.g}, ${color.b})`;
+})
